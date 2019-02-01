4841a0a23f2b9487df59b2bb7e195da62aa1612f
carol-df <30900333+carol-df@users.noreply.github.com>
2019-02-01 10:29:19 +0000

    updated index.js
    added more counties and stations

--- lambda/eu-west-1_aws-serverless-repository-alexaskillskitnodejsfact-OY64WWKQZMQM/index.js
+++ lambda/eu-west-1_aws-serverless-repository-alexaskillskitnodejsfact-OY64WWKQZMQM/index.js
@@ -159,9 +159,6 @@ const GetTidalStationsHandler = {
         case 'CORNWALL':
           COUNTY_STATIONS = CORNWALL_STATIONS;
           break;
-        case 'BRISTOL':
-          COUNTY_STATIONS = BRISTOL_STATIONS;
-          break;
         case 'BUCKINGHAMSHIRE':
           COUNTY_STATIONS = BUCKS_STATIONS;
           break;
@@ -195,11 +192,68 @@ const GetTidalStationsHandler = {
         case 'GLOUCESTERSHIRE':
           COUNTY_STATIONS = GLOUC_STATIONS;
           break;
+        case 'GREATER LONDON':
+          COUNTY_STATIONS = GLON_STATIONS;
+          break;
+        case 'HAMPSHIRE':
+          COUNTY_STATIONS = HAMP_STATIONS;
+          break;
+        case 'HERTFORDSHIRE':
+          COUNTY_STATIONS = HERT_STATIONS;
+          break;
+        case 'ISLE OF WIGHT':
+          COUNTY_STATIONS = IOW_STATIONS;
+          break;
+        case 'KENT':
+          COUNTY_STATIONS = KENT_STATIONS;
+          break;
+        case 'LANCASHIRE':
+          COUNTY_STATIONS = LANC_STATIONS;
+          break;
+        case 'LINCOLNSHIRE':
+          COUNTY_STATIONS = LINC_STATIONS;
+          break;
+        case 'MERSEYSIDE':
+          COUNTY_STATIONS = MERSEY_STATIONS;
+          break;
+        case 'NORFOLK':
+          COUNTY_STATIONS = NORFOLK_STATIONS;
+          break;
+        case 'NORTH YORKSHIRE':
+          COUNTY_STATIONS = NYORKS_STATIONS;
+          break;
+        case 'NORTHAMPTON':
+          COUNTY_STATIONS = NHAMP_STATIONS;
+          break;
+        case 'NORTHUMBERLAND':
+          COUNTY_STATIONS = NHUMB_STATIONS;
+          break;
+        case 'OXFORDSHIRE':
+          COUNTY_STATIONS = OXFORD_STATIONS;
+          break;
+        case 'SOMERSET':
+          COUNTY_STATIONS = SOMER_STATIONS;
+          break;
+        case 'SUFFOLK':
+          COUNTY_STATIONS = SUFFOLK_STATIONS;
+          break;
+        case 'SURREY':
+          COUNTY_STATIONS = SURREY_STATIONS;
+          break;
+        case 'TYNE AND WEAR':
+          COUNTY_STATIONS = TYNE_STATIONS;
+          break;
+        case 'WEST SUSSEX':
+          COUNTY_STATIONS = WSUSSEX_STATIONS;
+          break;
+        case 'WORCESTERSHIRE':
+          COUNTY_STATIONS = WORCES_STATIONS;
+          break;  
       }
       let CURRENT_INDEX = 0;
       while (CURRENT_INDEX < COUNTY_STATIONS.length) {
         if(CURRENT_INDEX==COUNTY_STATIONS.length -1) {
-          prompt = prompt + COUNTY_STATIONS[CURRENT_INDEX].Name;
+          prompt = prompt + COUNTY_STATIONS[CURRENT_INDEX].Name + '. ';
         } else {
           prompt = prompt + COUNTY_STATIONS[CURRENT_INDEX].Name+ ', ';
         }
@@ -326,10 +380,9 @@ const ErrorHandler = {
 
 const SKILL_NAME = 'Tide Times UK';
 const HELP_MESSAGE = 'You can say: ask tide times for stations in Cornwall. You can say: ask tide times for tide times in Bude. You can say exit... What can I help you with?';
-const COUNTIES_HELP_MESSAGE = 'Currently the available counties are: Cornwall, Bristol, Buckinghamshire, Cambridgeshire, Cheshire, County Durham, Cumberland, Devon, Dorset, East Riding of Yorkshire, East Sussex, Essex and Gloucestershire.';
+const COUNTIES_HELP_MESSAGE = 'Currently the available counties are those in England.';
 const HELP_REPROMPT = 'You can say: ask tide times for stations in Cornwall. You can say: ask tide times for tide times in Bude. You can say exit..';
 const STOP_MESSAGE = 'Goodbye!';
-const BRISTOL_STATIONS=[{"Name":"Cumberland Basin Entrance","Id":"0524"},{"Name":"AVONMOUTH","Id":"0523"},{"Name":"Sea Mills","Id":"0523B"},{"Name":"Shirehampton","Id":"0523A"},{"Name":"White House","Id":"0521"}];
 const BUCKS_STATIONS=[{"Name":"Langstone Harbour","Id":"0066"}];
 const CAMBS_STATIONS=[{"Name":"Wisbech","Id":"0164A"}];
 const CHES_STATIONS=[{"Name":"Bee Ness","Id":"0108A"},{"Name":"Chester","Id":"0462"},{"Name":"Darnett Ness","Id":"0108C"},{"Name":"Fiddler's Ferry","Id":"0456A"},{"Name":"Widnes","Id":"0456"}];
@@ -346,7 +399,7 @@ const GLON_STATIONS=[{"Name":"Albert Bridge","Id":"0114"},{"Name":"Chelsea Bridg
 const HAMP_STATIONS=[{"Name":"Bucklers Hard","Id":"0042"},{"Name":"Bursledon","Id":"0063B"},{"Name":"Calshot Castle","Id":"0061"},{"Name":"Hurst Point","Id":"0039"},{"Name":"Lee-On-The-Solent","Id":"0064"},{"Name":"LYMINGTON","Id":"0040"},{"Name":"Northney","Id":"0068A"},{"Name":"PORTSMOUTH","Id":"0065"},{"Name":"PORTSMOUTH HIGH WATER STAND","Id":"0065A"},{"Name":"SOUTHAMPTON","Id":"0062"},{"Name":"WARSASH","Id":"0063A"}];
 const HERT_STATIONS=[{"Name":"Jupiter Point","Id":"0014E"}];
 const IOW_STATIONS=[{"Name":"Bembridge Approaches","Id":"0053B"},{"Name":"Bembridge Harbour","Id":"0054"},{"Name":"COWES","Id":"0060"},{"Name":"Freshwater Bay","Id":"0048"},{"Name":"Newport","Id":"0060B"},{"Name":"Ryde","Id":"0058"},{"Name":"Sandown","Id":"0053"},{"Name":"Totland Bay","Id":"0046"},{"Name":"Ventnor","Id":"0051"}];
-const KENT_STATIONS=[{"Name":"Bartlett Creek","Id":"0108B"},{"Name":"Allington Lock","Id":"0109E"},{"Name":"Bramble Creek","Id":"0129A"},{"Name":"Broadstairs","Id":"0102A"},{"Name":"Chatham","Id":"0109"},{"Name":"Deal","Id":"0098"},{"Name":"DOVER","Id":"0089"},{"Name":"Dungeness","Id":"0087"},{"Name":"Faversham","Id":"0107"},{"Name":"Folkestone","Id":"0088"},{"Name":"Gravesend","Id":"0111A"},{"Name":"Herne Bay","Id":"0104"},{"Name":"MARGATE","Id":"0103"},{"Name":"New Hythe","Id":"0104"},{"Name":"RAMSGATE","Id":"0102"},{"Name":"Richborough","Id":"0099"},{"Name":"Rochester","Id":"0109B"},{"Name":"SHEERNESS","Id":"0108"},{"Name":"Shivering Sand","Id":"0116A"},{"Name":"Upnor","Id":"0109A"},{"Name":"Whitstable Approaches","Id":"0105"},{"Name":"Wouldham","Id":"0109C"}];
+const KENT_STATIONS=[{"Name":"Bartlett Creek","Id":"0108B"},{"Name":"Allington Lock","Id":"0109E"},{"Name":"Bramble Creek","Id":"0129A"},{"Name":"Broadstairs","Id":"0102A"},{"Name":"Chatham","Id":"0109"},{"Name":"Deal","Id":"0098"},{"Name":"DOVER","Id":"0089"},{"Name":"Dungeness","Id":"0087"},{"Name":"Faversham","Id":"0107"},{"Name":"Folkestone","Id":"0088"},{"Name":"Gravesend","Id":"0111A"},{"Name":"Herne Bay","Id":"0104"},{"Name":"MARGATE","Id":"0103"},{"Name":"New Hythe","Id":"0109D"},{"Name":"RAMSGATE","Id":"0102"},{"Name":"Richborough","Id":"0099"},{"Name":"Rochester","Id":"0109B"},{"Name":"SHEERNESS","Id":"0108"},{"Name":"Shivering Sand","Id":"0116A"},{"Name":"Upnor","Id":"0109A"},{"Name":"Whitstable Approaches","Id":"0105"},{"Name":"Wouldham","Id":"0109C"}];
 const LANC_STATIONS=[{"Name":"Blackpool","Id":"0445"},{"Name":"Fleetwood","Id":"0444"},{"Name":"Glasson Dock","Id":"0442"},{"Name":"Haws Point","Id":"0439B"},{"Name":"Heysham","Id":"0441"},{"Name":"Lancaster","Id":"0442A"},{"Name":"Morecambe","Id":"0440B"},{"Name":"Preston","Id":"0446"},{"Name":"Wyre Lighthouse","Id":"0443"}];
 const LINC_STATIONS=[{"Name":"BOSTON","Id":"0166"},{"Name":"Bull Sand Fort","Id":"0171A"},{"Name":"Burton Stather","Id":"0177"},{"Name":"FLIXBOROUGH WHARF","Id":"0177A"},{"Name":"Humber Bridge","Id":"0176"},{"Name":"IMMINGHAM","Id":"0173"},{"Name":"Keadby","Id":"0178"},{"Name":"Lawyer's Creek","Id":"0164B"},{"Name":"Outer Westmark Knock","Id":"0163"},{"Name":"Owston Ferry","Id":"0178A"},{"Name":"Port Sutton Bridge","Id":"0164"},{"Name":"Skegness","Id":"0167"},{"Name":"Wisbech Cut","Id":"0163A"}];
 const MERSEY_STATIONS=[{"Name":"Formby","Id":"0448"},{"Name":"Garston","Id":"0452A"},{"Name":"Hilbre Island","Id":"0461"},{"Name":"LIVERPOOL","Id":"0451"},{"Name":"Southport","Id":"0447"}];
@@ -355,7 +408,7 @@ const NYORKS_STATIONS=[{"Name":"Filey Bay","Id":"0182"},{"Name":"Middlesbrough",
 const NHAMP_STATIONS=[{"Name":"Exmouth","Id":"0026B"},{"Name":"Foreland","Id":"0053A"},{"Name":"Grovehurst Jetty","Id":"0106"},{"Name":"Halfway Shoal","Id":"0439C"},{"Name":"HUMBER SEA TERMINAL","Id":"0172A"},{"Name":"Inner Dowsing Light","Id":"0168"},{"Name":"Inward Rocks","Id":"0519"},{"Name":"Minsmere Sluice","Id":"0139A"},{"Name":"Narlwood Rocks","Id":"0520"},{"Name":"Orford Haven Bar","Id":"0136"},{"Name":"RIVER TEES ENTRANCE","Id":"0185"},{"Name":"Seacombe","Id":"0452"},{"Name":"Stansore Point","Id":"0043"},{"Name":"Sunk Dredged Channel","Id":"0171B"},{"Name":"Tabs Head","Id":"0165"}];
 const NHUMB_STATIONS=[{"Name":"Amble","Id":"0206"},{"Name":"Berwick","Id":"0209"},{"Name":"BLYTH","Id":"0204"},{"Name":"Coquet Island","Id":"0205"},{"Name":"Holy Island","Id":"0208"},{"Name":"North Sunderland","Id":"0207"}];
 const OXFORD_STATIONS=[{"Name":"Folly Inn","Id":"0060A"}];
-const SOMER_STATIONS=[{"Name":"Bridgwater","Id":"0529"},{"Name":"Burnham-On-Sea","Id":"0528"},{"Name":"Clevedon","Id":"0525"},{"Name":"English And Welsh Grounds","Id":"0526"},{"Name":"Hinkley Point","Id":"0530"},{"Name":"Minehead","Id":"0532"},{"Name":"Porlock Bay","Id":"0533"},{"Name":"Portishead","Id":"0524A"},{"Name":"S.E. Long Sand","Id":"0117"},{"Name":"St. Thomas's Head","Id":"0525A"},{"Name":"Watchet","Id":"0531"},{"Name":"Wells","Id":"0157A"},{"Name":"West Stones","Id":"0161A"},{"Name":"Weston-Super-Mare","Id":"0527"}];
+const SOMER_STATIONS=[{"Name":"AVONMOUTH","Id":"0523"},{"Name":"Bridgwater","Id":"0529"},{"Name":"Burnham-On-Sea","Id":"0528"},{"Name":"Clevedon","Id":"0525"},{"Name":"Cumberland Basin Entrance","Id":"0524"},{"Name":"English And Welsh Grounds","Id":"0526"},{"Name":"Hinkley Point","Id":"0530"},{"Name":"Minehead","Id":"0532"},{"Name":"Porlock Bay","Id":"0533"},{"Name":"Portishead","Id":"0524A"},{"Name":"Sea Mills","Id":"0523B"},{"Name":"Shirehampton","Id":"0523A"},{"Name":"S.E. Long Sand","Id":"0117"},{"Name":"St. Thomas's Head","Id":"0525A"},{"Name":"Watchet","Id":"0531"},{"Name":"Wells","Id":"0157A"},{"Name":"West Stones","Id":"0161A"},{"Name":"Weston-Super-Mare","Id":"0527"},{"Name":"White House","Id":"0521"}];
 const SUFFOLK_STATIONS=[{"Name":"Aldeburgh","Id":"0139"},{"Name":"Bawdsey","Id":"0135"},{"Name":"FELIXSTOWE PIER","Id":"0133A"},{"Name":"Iken Cliffs","Id":"0136C"},{"Name":"Ipswich","Id":"0133"},{"Name":"LOWESTOFT","Id":"0141"},{"Name":"Orford Ness","Id":"0137"},{"Name":"Orford Quay","Id":"0136A"},{"Name":"Slaughden Quay","Id":"0136B"},{"Name":"Southwold","Id":"0140"},{"Name":"Woodbridge","Id":"0134A"},{"Name":"Woodbridge Haven","Id":"0134"}];
 const SURREY_STATIONS=[{"Name":"Hammersmith Bridge","Id":"0115"},{"Name":"Kew Bridge","Id":"0115A"},{"Name":"Richmond Lock","Id":"0116"}];
 const TYNE_STATIONS=[{"Name":"Newcastle-Upon-Tyne","Id":"0203"},{"Name":"NORTH SHIELDS","Id":"0202"},{"Name":"SUNDERLAND","Id":"0190"}];
